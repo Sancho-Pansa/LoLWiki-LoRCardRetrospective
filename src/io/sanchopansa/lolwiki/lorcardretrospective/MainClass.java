19aabd0b2ff8823a37f07b9c21ef6f060b7020ae
SanchoPansa <zelgadis4@gmail.com>
2023-06-14 18:32:29 +0300

    Picocli Test

--- src/io/sanchopansa/lolwiki/lorcardretrospective/MainClass.java
+++ src/io/sanchopansa/lolwiki/lorcardretrospective/MainClass.java
@@ -1,10 +1,26 @@
 package io.sanchopansa.lolwiki.lorcardretrospective;
 
-public class MainClass {
+import picocli.CommandLine;
+
+import java.io.File;
+
+public class MainClass implements Runnable {
+    @CommandLine.Option(names= {"-f", "--file"}, paramLabel = "File", description = "output file", arity = "0..1")
+    private static File file;
+
+    @CommandLine.Parameters(index = "0", paramLabel = "Card code", description = "LoR Card ID")
+    private static String cardCode;
     public static void main(String[] args) {
+        new CommandLine(new MainClass()).execute(args);
         System.out.println("Ныне и присно возношу молитву во имя Изменяющего Пути, Великого Заговорщика и Архитектора Судеб.");
+        System.out.println(cardCode);
         LoRCardRetrospector retrospector = new LoRCardRetrospector("05IO003");
-        retrospector.getCardRetrospection();
-        retrospector.printCardHistory();
+        //retrospector.getCardRetrospection();
+        //retrospector.printCardHistory();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
